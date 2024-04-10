23b725bd95c3854fbd75c6fac7335f7e2c399b26
Pranay50x <2005pranaykr@gmail.com>
2024-04-10 17:59:46 +0530

    added spotlight to the site

---
+++ musicnextjs/src/components/HeroSection.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/no-unescaped-entities */
+import Link from 'next/link'
+import React from 'react'
+import { Spotlight } from './ui/spotlight'
+
+function HeroSection() {
+  return (
+    <div
+    className='h-auto md:h-[40rem] w-full flex rounded-m flex-col justify-center items-center relative overflow-hidden mx-auto py-10 md:0  '>
+      <div className='relative p-4 w-full text-center'>
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+            <h1 className='mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
+            '>Master the art of Music</h1>
+            <p className='mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto'>
+                Dive into our comprehensive music courses and transform your musical journey today. 
+                Whether you're a beginner or looking to refine your skills, join us to unlock your full potential.</p>
+            <div className='mt-4'>
+                <Link href={'/courses'}>
+                    Explore Courses
+                </Link>
+            </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
